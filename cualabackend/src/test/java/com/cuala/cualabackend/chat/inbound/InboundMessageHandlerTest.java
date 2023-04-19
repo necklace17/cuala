43c35fa531e375136f5098b63931a86ced684b90
necklace17 <niklas.scholz@outlook.com>
2023-04-19 15:45:08 +0800

    Improved unit test

--- cualabackend/src/test/java/com/cuala/cualabackend/chat/inbound/InboundMessageHandlerTest.java
+++ cualabackend/src/test/java/com/cuala/cualabackend/chat/inbound/InboundMessageHandlerTest.java
@@ -1,5 +1,6 @@
 package com.cuala.cualabackend.chat.inbound;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -8,9 +9,13 @@ import com.cuala.cualabackend.chat.application.MessageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketMessage.Type;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -23,45 +28,63 @@ class InboundMessageHandlerTest {
 
   @Mock
   private InboundTemplateEngine inboundTemplateEngine;
+  @InjectMocks
+  private InboundMessageHandler inboundMessageHandler;
 
-
-  private InboundMessageHandler handler;
 
   @BeforeEach
   public void setUp() {
     MockitoAnnotations.openMocks(this);
-    handler = new InboundMessageHandler(messageService, inboundTemplateEngine);
+
   }
+
 
   @Test
   public void handle_shouldProcessMessageAndReturnResponse() {
-    // mock input
+    // Arrange
     String messageFromClient = "hello";
+    WebSocketSession webSocketSession = createWebSocketSessionFromMessage(messageFromClient);
+    var successMessage = "Success " + messageFromClient;
+    mockDependencies(messageFromClient, successMessage);
+    // Act
+    inboundMessageHandler.handle(webSocketSession);
+    // Assert
+    ArgumentCaptor<Flux<WebSocketMessage>> argumentCaptor = ArgumentCaptor.forClass(Flux.class);
+    verify(webSocketSession).send(argumentCaptor.capture());
+    StepVerifier.create(argumentCaptor.getValue()
+            .map(WebSocketMessage::getPayloadAsText))
+        .expectNext(successMessage)
+        .verifyComplete();
+  }
+
+  private WebSocketSession createWebSocketSessionFromMessage(String messageFromClient) {
+    Flux<WebSocketMessage> incomingFlux = createIncomingFlux(messageFromClient);
+    return createWebSocketSession(incomingFlux);
+  }
+
+  private void mockDependencies(String messageFromClient, String successMessage) {
+    when(messageService.processMessage(messageFromClient)).thenReturn(
+        Mono.just(messageFromClient));
+    when(inboundTemplateEngine.buildSuccessfulResponse(messageFromClient)).thenReturn(
+        successMessage);
+  }
+
+  private static Flux<WebSocketMessage> createIncomingFlux(String messageFromClient) {
     WebSocketMessage incomingMessage = mock(WebSocketMessage.class);
     when(incomingMessage.getPayloadAsText()).thenReturn(messageFromClient);
-    Flux<WebSocketMessage> incomingFlux = Flux.just(incomingMessage);
+    return Flux.just(incomingMessage);
+  }
+
+  private static WebSocketSession createWebSocketSession(Flux<WebSocketMessage> incomingFlux) {
     WebSocketSession webSocketSession = mock(WebSocketSession.class);
     when(webSocketSession.receive()).thenReturn(incomingFlux);
-
-    var anotherMessage = mock(WebSocketMessage.class);
-    when(anotherMessage.getPayloadAsText()).thenReturn("You did it");
-    when(webSocketSession.textMessage("Response")).thenReturn(anotherMessage);
-
-    // mock service response and template engine
-    when(messageService.processMessage(messageFromClient)).thenReturn(Mono.just("world"));
-    when(inboundTemplateEngine.buildSuccessfulResponse("world"))
-        .thenReturn("Response");
-
-    // Act
-    handler.handle(webSocketSession);
-    // Assert
-    ArgumentCaptor<Flux<WebSocketMessage>> ac =
-        ArgumentCaptor.forClass(Flux.class);
-    verify(webSocketSession).send(ac.capture());
-    var transformedClientMessage = ac.getValue()
-        .map(WebSocketMessage::getPayloadAsText);
-    StepVerifier.create(transformedClientMessage)
-        .expectNext("You did it")
-        .verifyComplete();
+    DataBufferFactory dataBufferFactory = new DefaultDataBufferFactory();
+    when(webSocketSession.textMessage(anyString()))
+        .thenAnswer(invocation -> {
+          String arg = invocation.getArgument(0);
+          return new WebSocketMessage(Type.TEXT,
+              dataBufferFactory.wrap(arg.getBytes()));
+        });
+    return webSocketSession;
   }
 }
